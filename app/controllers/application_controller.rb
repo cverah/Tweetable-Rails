eadf542e20491ffdb34a6ecce2a77297236c2474
Cristhian Vera <cristianmario092190@gmail.com>
2023-10-29 15:58:32 -0500

    feat: add gem devise for authentication in tweetable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  # atehenticate
+  before_action :authenticate_user!
+  # para agregar parametros al new_loggin
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # pandit
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  def pundit_user
+    current_user
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back(fallback_location: root_path)
+  end
+
 end
---
+++ db/migrate/20230729201944_create_api_users.rb
@@ -0,0 +1,15 @@
+class CreateApiUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :api_users do |t|
+      t.string :email
+      t.string :username
+      t.string :name
+      t.integer :role, default: 0
+      t.string :password_digest
+      t.string :token
+
+      t.timestamps
+    end
+    add_index :api_users, :token, unique: true
+  end
+end
